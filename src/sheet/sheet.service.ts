751455019fdaf2da26351d97ea8186067e732e9b
Saksit Sirisakda <saksit.sirisakda@cr3.group>
2025-07-15 15:18:55 +0700

    add append method

--- src/sheet/sheet.service.ts
+++ src/sheet/sheet.service.ts
@@ -14,7 +14,7 @@ export class SheetService implements OnModuleInit {
       process.env.GOOGLE_SPREADSHEET_ID || 'YOUR_SPREADSHEET_ID_HERE';
 
     const auth = new google.auth.GoogleAuth({
-      scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
       keyFile,
     });
     const authClient = await auth.getClient();
@@ -23,6 +23,17 @@ export class SheetService implements OnModuleInit {
 
     // Example usage: Fetch data from a specific range in the spreadsheet
     const range = 'sheet1!A2:C';
+
+    try {
+      await this.writeSheetData(spreadsheetId, range, [
+        ['New Data 1', 'New Data 2', 'New Data 3'],
+        ['More Data 1', 'More Data 2', 'More Data 3'],
+      ]);
+    } catch (e) {
+      console.error('Error writing data to the sheet:', e);
+      return;
+    }
+
     const raws = await this.getSheetData(spreadsheetId, range);
     console.log(raws);
   }
@@ -34,5 +45,21 @@ export class SheetService implements OnModuleInit {
     });
 
     return res.data.values;
+  }
+
+  async writeSheetData(spreadsheetId: string, range: string, values: any[][]) {
+    const res = await this.sheets.spreadsheets.values.append({
+      spreadsheetId,
+      range,
+      valueInputOption: 'USER_ENTERED',
+      requestBody: {
+        values,
+      },
+    });
+
+    if (res.ok) {
+      return res.data;
+    }
+    throw new Error('Failed to write data to the sheet');
   }
 }
